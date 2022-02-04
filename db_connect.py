741390c447b1a0dc3de81625262b9ec169aa97f5
Ehtesham <ehtesham599@gmail.com>
2022-02-05 00:01:23 +0300

    Added db creds as env variables

--- db_connect.py
+++ db_connect.py
@@ -1,4 +1,10 @@
 import mysql.connector
+from decouple import config
+
+db_host = config('DB_HOST')
+db_user = config('DB_USER')
+db_passwd = config('DB_PASSWD')
+db_database_name = config('DB_DATABASE_NAME')
 
 db = mysql.connector.connect(
-    host='127.0.0.1', user='root', passwd='17bce1292', database='ehtesham')
+    host=db_host, user=db_user, passwd=db_passwd, database=db_database_name)
